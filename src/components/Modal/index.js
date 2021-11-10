b9bcd534211ccec00d90387e3381c1751596c2a7
Kirill.Alekseev <kirill.alekseev@x5.ru>
2021-11-10 15:01:52 +0300

    refactor useEffect

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,39 +1,42 @@
-﻿import React, { useState, useRef, useEffect, useCallback } from 'react';
+﻿import React, { useRef, useEffect, useCallback, useState } from 'react';
 import ModalHeader from './ModalHeader';
 import ModalBody from './ModalBody';
 import ModalFooter from './ModalFooter';
 import { createPortal } from 'react-dom';
 import './modal.scss';
 
-const Modal = ({show, children, onHide, closeOutside = true}) => {
+const Modal = ({show, children, onHide, closeOutside = true, width = null}) => {
+  //onAnimationEnd/onAnimationStart
   const modalRef = useRef();
   const className = show ? `modal-clever active` : 'modal-clever';
   const handleUserKeyPress = useCallback(e => {
+    console.log('handleUserKeyPress');
     if (e.code === 'Escape' && onHide){
       onHide(false);
     }
   }, []);
 
   const clickOutside = (e) => {
+    console.log('clickOutside');
     if (closeOutside && e.target === modalRef.current && onHide) {
       onHide(false);
     }
   };
 
   useEffect(() => {
+    console.log('START USE EFFECT')
     window.addEventListener("keydown", handleUserKeyPress);
-    return () => window.removeEventListener("keydown", handleUserKeyPress);
-  }, [handleUserKeyPress]);
-
-  //Объединить возможно useEffect?
-  useEffect(() => {
-     window.addEventListener("mousedown", clickOutside);
-    return () => window.removeEventListener("mousedown", clickOutside);
-  }, [clickOutside]);
+    window.addEventListener("mousedown", clickOutside);
+    return () => {
+      window.removeEventListener("keydown", handleUserKeyPress);
+      window.removeEventListener("mousedown", clickOutside);
+      console.log('END USE EFFECT')
+    }
+  }, [clickOutside, handleUserKeyPress]);
 
   return createPortal(
     <div className={className} ref={modalRef}>
-      <div className="modal-clever-content">
+      <div className="modal-clever-content" style={{width: width}}>
         {children}
       </div>
     </div>,
--- src/components/index.js
+++ src/components/index.js
@@ -7,17 +7,35 @@ const App = () => {
   return (
     <div style={{background: '#EFF1F5', padding: '30px'}}>
       <button onClick={() => setOpen(true)}>OPEN MODAL</button>
-      <Modal show={open} onHide={setOpen}>
+      <Modal show={open} onHide={setOpen} width="400px">
         <Modal.Header> 
           <div>ЗАГОЛОВОК</div>
         </Modal.Header>
         <Modal.Body>
-          <p>Create React App is a comfortable environment for learning React, 
+          <div>Create React App is a comfortable environment for learning React, 
             and is the best way to start building a new single-page application in React.
             It sets up your development environment so that you can use the latest JavaScript features, 
             provides a nice developer experience, and optimizes your app for production. You’ll need to have Node 
             >= 14.0.0 and npm >= 5.6 on your machine. To create a project, run:
-          </p>
+          </div>
+          <div>Create React App is a comfortable environment for learning React, 
+            and is the best way to start building a new single-page application in React.
+            It sets up your development environment so that you can use the latest JavaScript features, 
+            provides a nice developer experience, and optimizes your app for production. You’ll need to have Node 
+            >= 14.0.0 and npm >= 5.6 on your machine. To create a project, run:
+          </div>
+          <div>Create React App is a comfortable environment for learning React, 
+            and is the best way to start building a new single-page application in React.
+            It sets up your development environment so that you can use the latest JavaScript features, 
+            provides a nice developer experience, and optimizes your app for production. You’ll need to have Node 
+            >= 14.0.0 and npm >= 5.6 on your machine. To create a project, run:
+          </div>
+          <div>Create React App is a comfortable environment for learning React, 
+            and is the best way to start building a new single-page application in React.
+            It sets up your development environment so that you can use the latest JavaScript features, 
+            provides a nice developer experience, and optimizes your app for production. You’ll need to have Node 
+            >= 14.0.0 and npm >= 5.6 on your machine. To create a project, run:
+          </div>
           <button onClick={() => setOpen(false) }>CLOSE MODAL</button>
         </Modal.Body>
         <Modal.Footer>
